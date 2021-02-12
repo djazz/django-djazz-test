2e1f4da0c6c2bc737c745837c5a03e529a500a53
Pascal Polleunus <pascal+github@polleunus.be>
2021-02-12 15:10:26 +0100

    Move to dedicated repo.

--- project/docker/app/scripts/docker-install.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-# Install the `app` service.
-
-
-echo "Installing the app (app mode: $APP_MODE)..."
-
-# Install pip packages.
-pip install --no-cache-dir --upgrade pip setuptools
-/home/django/pip-install.sh
